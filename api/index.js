a47ba7d145175981801aea1318f132d844cbcb3c
Sílvia Mur Blanch <pchiwan@gmail.com>
2019-12-30 15:22:51 +0100

    Add index.html to prevent directory listing

--- api/index.js
+++ api/index.js
@@ -2,5 +2,5 @@ const { HTTP_OK } = require('../src/constants');
 
 module.exports = function (req, res) {
   res.status(HTTP_OK);
-  res.send('<h1>To infinity and beyond!</h1>');
+  res.send('Hello world! 👋');
 };
